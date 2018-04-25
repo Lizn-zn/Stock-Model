dc27348eba31f9650126a8842511ef36dde13804
lizenan <32972438+Lizenan1995@users.noreply.github.com>
2018-04-25 14:19:42 +0800

    update download

--- data_download/data_download.py
+++ data_download/data_download.py
@@ -1,5 +1,4 @@
 # _*_ coding:utf-8 _*_
-# name gefile.py
 import sys
 import os
 import stat
@@ -8,39 +7,41 @@ import paramiko
 
 
 def data_download():
-	FILES = ["估值指标", "成长能力与偿债能力", "技术指标", "财务质量",  "收盘价格"]
-	USERNAME = "root"
-	PASSWORD = "FhWJ4J6k"
-	HOST = "210.28.133.11"
-	PORT = 21030
-	remotefile = "/root/things/"
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.connect((HOST, PORT))
-	t = paramiko.Transport(sock)
-	t.start_client()
-	t.auth_password(USERNAME, PASSWORD)
-	sftp = paramiko.SFTPClient.from_transport(t)
-	for f in FILES:
-	    if not os.path.exists(f):
-	        os.makedirs(f)
-	    localdir = sys.path[0] + "\\" + f
-	    remotedir = remotefile + f
-	    files = sftp.listdir_attr(remotedir)
-	    for temp in files:
-	        remotepath = remotedir + "/" + temp.filename
-	        localpath = localdir + "\\" + temp.filename
-	        if(os.path.exists(localpath)):
-	            print('exists {}, continue...'.format(temp.filename))
-	            continue
-	        sftp.get(remotepath, localpath)
-	        print('download {}...'.format(temp.filename))
-	sftp.close()
-	t.close()
-	sock.close()
+    FILES = ["估值指标", "成长能力与偿债能力", "技术指标", "财务质量",  "收盘价格"]
+    USERNAME = "root"
+    PASSWORD = "FhWJ4J6k"
+    HOST = "210.28.133.11"
+    PORT = 21030
+    remotefile = "/root/things/"
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
+    t = paramiko.Transport(sock)
+    t.start_client()
+    t.auth_password(USERNAME, PASSWORD)
+    sftp = paramiko.SFTPClient.from_transport(t)
+    for f in FILES:
+        if not os.path.exists(f):
+            os.makedirs(f)
+        localdir = sys.path[0] + "\\" + f
+        remotedir = remotefile + f
+        files = sftp.listdir_attr(remotedir)
+        for temp in files:
+            # Continue if it is not a table
+            if not (temp.endswith(".csv") or temp.endswith(".xlsx")):
+                continue
+            elif temp.startswith("~$"):
+                continue
+            remotepath = remotedir + "/" + temp.filename
+            localpath = localdir + "\\" + temp.filename
+            if(os.path.exists(localpath)):
+                print('exists {}, continue...'.format(temp.filename))
+                continue
+            sftp.get(remotepath, localpath)
+            print('download {}...'.format(temp.filename))
+    sftp.close()
+    t.close()
+    sock.close()
 
 
 if __name__ == '__main__':
-	data_download()
-
-
-
+    data_download()
